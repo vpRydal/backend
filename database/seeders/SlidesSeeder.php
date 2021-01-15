a59ea088b937165112a18490b410bcb2d1db7a58
Lutsenko Evgenii <luthenkoev@gmail.com>
2021-01-15 17:30:06 +0300

    get Slides

---
+++ database/seeders/SlidesSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Slide;
+use Illuminate\Database\Seeder;
+
+class SlidesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $slide1=new Slide();
+        $slide1->header="что-то у слайдера";
+        $slide1->content="что-то у слайдера это основной контент";
+        $slide1->image="/storage/image/image.jpg";
+        $slide1->user_id=1;
+        $slide1->save();
+
+        $slide2=new Slide();
+        $slide2->header="что-то у слайдера";
+        $slide2->content="что-то у слайдера это основной контент";
+        $slide2->image="/storage/image/image.jpg";
+        $slide2->user_id=1;
+        $slide2->save();
+    }
+}
